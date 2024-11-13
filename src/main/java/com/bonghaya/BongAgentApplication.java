7454f3482eee89d410787875938ff8291ef687ad
bong-ha Hwang <bonghaya@gmail.com>
2024-11-13 21:26:42 +0900

    main 1st commit

---
+++ src/main/java/com/bonghaya/BongAgentApplication.java
@@ -0,0 +1,13 @@
+package com.bonghaya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BongAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BongAgentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bonghaya/BongAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bonghaya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BongAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
